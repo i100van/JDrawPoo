da4df132e88dd42bb40539c05708b596b9a36390
i100van <i100van@hotmail.com>
2019-11-08 18:52:31 +0100

    GestorDelPrompt

---
+++ JDraw/src/Excepciones/Comando_no_valido.java
@@ -0,0 +1,7 @@
+package Excepciones;
+
+public class Comando_no_valido extends Exception {
+    public Comando_no_valido() {
+        super("Comando no valido");
+    }
+}
---
+++ JDraw/src/Excepciones/NoEsEnteroExcepcion.java
@@ -0,0 +1,8 @@
+package Excepciones;
+
+public class NoEsEnteroExcepcion extends Exception{
+    public NoEsEnteroExcepcion() {
+        super("No es un entero");
+    }
+
+}
