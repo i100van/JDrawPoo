83d4737fe9bc0fb0c0d25d5fbee557ae737809b3
i100van <i100van@hotmail.com>
2019-11-08 20:40:05 +0100

    Prompt terminado

--- JDraw/src/InterfazUsuario.java
+++ JDraw/src/InterfazUsuario.java
@@ -3,33 +3,39 @@ import Comandos.GestorPrompt;
 public class InterfazUsuario {
     public InterfazUsuario (){
         boolean fin=false;
+        GestorPrompt prompt = new GestorPrompt();
         while(!fin){
-            GestorPrompt prompt = new GestorPrompt();
+            prompt.pedir_comando();
+            System.out.println(prompt.getlista_comandos());
             String comando=prompt.getComando();
             switch (comando){
                 case "line":
-
+                    System.out.println("Estoy dibujando linea");
+                    break;
                 case "rect":
-
+                    break;
                 case "circle":
-
+                    break;
                 case "text":
-
+                    break;
                 case "ellipse":
-
+                    break;
                 case "pencolor":
-
+                    break;
                 case "fillcolor":
-
+                    break;
                 case "width":
-
+                    break;
                 case "clear":
-
+                    break;
                 case "undo":
-
+                    break;
                 case "save":
-
+                    break;
                 case "load":
+                    break;
+                case "exit":
+                    fin=true;
 
             }
 
